5e7804db48f991f969d14daaa71d07285ebafb2d
Kaouthar Mokrane <kaouthar.mokrane@gmail.com>
2019-10-13 22:33:12 +0100

    dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Container(child: Text('Dicee'))),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void rollDice(){
+    leftDiceNumber = Random().nextInt(6)+1;//[1,6]
+    print('leftDiceNumber= $leftDiceNumber');
+    rightDiceNumber = Random().nextInt(6)+1 ;//[1,6]
+    print("rightDiceNumber= $rightDiceNumber");
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rollDice();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rollDice();
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
